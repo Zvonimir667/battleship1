64a4467d90b65f2f27d5bf37c73d8ae6610696d2
Zvonimir667 <>
2023-11-15 09:25:21 +0000

    Add docstring for functions

--- run.py
+++ run.py
@@ -5,6 +5,15 @@ scores = {"computer": 0, "player": 0}
 
 class Board:
     def __init__(self, size, num_ships, name, type):
+        """
+        Initialize a game board.
+
+        Parameters:
+        - size (int): The size of the square board.
+        - num_ships (int): The number of ships to be placed on the board.
+        - name (str): The name of the player or computer.
+        - type (str): The type of the board, either "player" or "computer".
+        """
         self.size = size
         self.board = [["." for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
@@ -14,10 +23,23 @@ class Board:
         self.ships = []
 
     def print(self):
+        """
+        Print the current state of the board.
+        """
         for row in self.board:
             print(" ".join(row))
 
     def guess(self, x, y):
+        """
+        Make a guess on the board and update the board state.
+
+        Parameters:
+        - x (int): The row index of the guess.
+        - y (int): The column index of the guess.
+
+        Returns:
+        - str: The result of the guess, either "Hit" or "Miss".
+        """
         self.guesses.append((x, y))
         self.board[x][y] = "X"
 
@@ -28,6 +50,14 @@ class Board:
             return "Miss"
 
     def add_ship(self, x, y, type="computer"):
+        """
+        Add a ship to the board at the specified coordinates.
+
+        Parameters:
+        - x (int): The row index to place the ship.
+        - y (int): The column index to place the ship.
+        - type (str): The type of the ship, default is "computer".
+        """
         if len(self.ships) >= self.num_ships:
             print("Error: you cannot add any more ships!")
         else:
@@ -35,14 +65,43 @@ class Board:
             if self.type == "player":
                 self.board[x][y] = "S"
 
+
 def random_point(size):
-	return randint(0, size -1)
+    """
+    Generate a random coordinate within the given size.
+
+    Parameters:
+    - size (int): The size of the board.
+
+    Returns:
+    - int: A random coordinate within the board size.
+    """
+	return randint(0, size - 1)
+
 
 def valid_coordinates(x, y, board):
+    """
+    Check if the given coordinates are valid for the board.
+
+    Parameters:
+    - x (int): The row index.
+    - y (int): The column index.
+    - board (Board): The game board.
+
+    Returns:
+    - bool: True if the coordinates are valid, False otherwise.
+    """
     size = board.size
     return 0 <= x < size and 0 <= y < size and (x, y) not in board.guesses
 
+
 def populate_board(board):
+    """
+    Populate the board with ships until the required number is reached.
+
+    Parameters:
+    - board (Board): The game board to populate.
+    """
     while len(board.ships) < board.num_ships:
         x = random_point(board.size)
         y = random_point(board.size)
@@ -52,7 +111,17 @@ def populate_board(board):
         else:
             continue
 
+
 def make_guess(board):
+    """
+    Get user input for a guess until a valid one is provided.
+
+    Parameters:
+    - board (Board): The game board.
+
+    Returns:
+    - tuple: A valid guess as a tuple (x, y).
+    """
     while True:
         try:
             x = int(input(f"Enter row (0-{board.size-1}): "))
@@ -62,11 +131,18 @@ def make_guess(board):
                 return x, y
             else:
                 print("Invalid guess. Try again.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 
 def play_game(computer_board, player_board):
+    """
+    Play the Battleship game.
+
+    Parameters:
+    - computer_board (Board): The computer's game board.
+    - player_board (Board): The player's game board.
+    """
     while True:
         print("\nYour Board:")
         player_board.print()
@@ -96,6 +172,9 @@ def play_game(computer_board, player_board):
 
 
 def new_game():
+    """
+    Start a new game of Battleship.
+    """
     size = 5
     num_ships = 4
     scores["computer"] = 0
@@ -116,5 +195,6 @@ def new_game():
         populate_board(computer_board)
 
     play_game(computer_board, player_board)
+
 
 new_game()
